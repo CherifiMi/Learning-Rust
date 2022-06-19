7fe27738f610f6ab01755b664f568a7c2649b17b
mito <mitodev2003@gmail.com>
2022-06-19 10:52:28 +0100

    first commit

---
+++ hello-cargo/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\mitod\Desktop\rust-learn\hello-cargo\hello_cargo\target\debug\hello_cargo.exe: C:\Users\mitod\Desktop\rust-learn\hello-cargo\hello_cargo\src\main.rs
---
+++ hello-cargo/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\mitod\Desktop\rust-learn\hello-cargo\hello_cargo\target\release\hello_cargo.exe: C:\Users\mitod\Desktop\rust-learn\hello-cargo\hello_cargo\src\main.rs
