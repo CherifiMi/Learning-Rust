403388fb74399a8cf4e1edf8b8d91e42bf14d5e7
mito <mitodev2003@gmail.com>
2022-06-19 11:03:21 +0100

    first run

---
+++ gussing_game/target/debug/deps/gussing_game.d
@@ -0,0 +1,5 @@
+C:\Users\mitod\Desktop\rust-learn\gussing_game\target\debug\deps\gussing_game.exe: src\main.rs
+
+C:\Users\mitod\Desktop\rust-learn\gussing_game\target\debug\deps\gussing_game.d: src\main.rs
+
+src\main.rs:
---
+++ gussing_game/target/debug/gussing_game.d
@@ -0,0 +1,1 @@
+C:\Users\mitod\Desktop\rust-learn\gussing_game\target\debug\gussing_game.exe: C:\Users\mitod\Desktop\rust-learn\gussing_game\src\main.rs
