a3aa354f45dab78737ef251cd6a3e3209f9e124d
mito <cherifi154949@gmail.com>
2022-06-20 01:39:13 +0100

    IJ test

---
+++ intt_test/target/debug/deps/intt_test.d
@@ -0,0 +1,5 @@
+C:\Users\mitod\Desktop\rust-learn\intt_test\target\debug\deps\intt_test.exe: src\main.rs
+
+C:\Users\mitod\Desktop\rust-learn\intt_test\target\debug\deps\intt_test.d: src\main.rs
+
+src\main.rs:
---
+++ intt_test/target/debug/intt_test.d
@@ -0,0 +1,1 @@
+C:\Users\mitod\Desktop\rust-learn\intt_test\target\debug\intt_test.exe: C:\Users\mitod\Desktop\rust-learn\intt_test\src\main.rs
