818958558de50669423827c2abfe9c6313d0d9d1
CherifiMi <cherifi154949@gmail.com>
2022-06-23 09:30:06 +0100

    deleted html

--- intt_test/target/debug/deps/intt_test.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mitod\Desktop\rust-learn\intt_test\target\debug\deps\intt_test.exe: src\main.rs
-
-C:\Users\mitod\Desktop\rust-learn\intt_test\target\debug\deps\intt_test.d: src\main.rs
-
-src\main.rs:
