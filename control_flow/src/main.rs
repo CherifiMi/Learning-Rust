46a18509f1dfa1e4a5c6b743452e0b94d1f15f36
CherifiMi <cherifi154949@gmail.com>
2022-07-05 15:34:06 +0100

    fizzbuzz

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+
+    let x = if false {3} else {6};
+
+    println!("{}",x);
+
+}
+
+fn _fizzbuzz(){
+    for i in 1..101 {
+
+        if i%3==0 && i%5==0 {println!("FizzBuzz")}
+        else if i%3==0 {println!("Fizz")}
+        else if i%5==0 {println!("Buzz")}
+        else {println!("{i}")}
+
+    }
+}
+
+fn ifs(){
+    let x = 10;
+
+    if x != 10 {
+        println!("Hello");
+    }else{
+        println!("world!");
+    }
 }
