18dacfa061afbbf0bd9a1296438a669b95b96f65
CherifiMi <cherifi154949@gmail.com>
2022-07-05 16:06:34 +0100

    fixes

--- control_flow/target/debug/control_flow.d
+++
@@ -1,1 +0,0 @@
-D:\mito\Resources\rust\rust-learn\control_flow\target\debug\control_flow.exe: D:\mito\Resources\rust\rust-learn\control_flow\src\main.rs
--- control_flow/target/debug/deps/control_flow.d
+++
@@ -1,5 +0,0 @@
-D:\mito\Resources\rust\rust-learn\control_flow\target\debug\deps\control_flow.exe: src\main.rs
-
-D:\mito\Resources\rust\rust-learn\control_flow\target\debug\deps\control_flow.d: src\main.rs
-
-src\main.rs:
