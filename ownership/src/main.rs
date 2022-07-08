ffbd0e89808ab0fbd4135c97acd4043845312d78
CherifiMi <cherifi154949@gmail.com>
2022-07-08 18:25:43 +0100

    return ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,15 +1,37 @@
 fn main() {
+    let s = String::from("hi");
 
-
+    do_thing(s.clone())
+}
+fn do_thing(s: String){
+    //..
 }
 
 
 
 
+//____________________________________________________________________
+fn _return_scope(){
+    let s1 = _gives_ownership();
+
+    let s2 = String::from("hello");
+
+    let s3 = _takes_and_gives_back(s2);
+
+    println!("{}", s3)
+}
+fn _takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+fn _gives_ownership() -> String {
 
 
+    let some_string = String::from("yours");
+
+    some_string
+}
 //_______________________________________________________________________
-fn _copy_clone_move(){
+fn _copy_clone_move() {
     let s1 = String::from("well hello there!!");
     let s2 = s1;
 
@@ -22,22 +44,25 @@ fn _copy_clone_move(){
     let d = 4;
     let b = d;
 
-    println!("  z:{}  s:{} d:{}", z1==z2, x==y, d==b);
+    println!("  z:{}  s:{} d:{}", z1 == z2, x == y, d == b);
 }
 
-fn _functions(){
+//_______________________________________________________________________
+fn _functions() {
     let s = String::from("mito");
 
-    takes_ownership(s);
+    _takes_ownership(s);
 
     let x = 5;
-    make_copy(x);
+    _make_copy(x);
 
     println!("{}", x);
 }
-fn takes_ownership(s: String){
+
+fn _takes_ownership(s: String) {
     println!("{}", s);
 }
-fn make_copy(x: i32){
+
+fn _make_copy(x: i32) {
     println!("{}", x);
 }
