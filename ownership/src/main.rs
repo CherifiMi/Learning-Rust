ccbf14e028daad130e789ca48eaac8d13f5d09f3
CherifiMi <cherifi154949@gmail.com>
2022-07-08 17:23:44 +0100

    function ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,16 +1,44 @@
 fn main() {
-    let mut s = String::from("well hello there!!");
-
-    let v = s;
 
 
+}
+
+
+
+
+
+
+//_______________________________________________________________________-
+fn _functions(){
+    let s = String::from("mito");
+
+    takes_ownership(s);
+
+    let x = 5;
+    make_copy(x);
+
+    println!("{}", x);
+}
+fn takes_ownership(s: String){
+    println!("{}", s);
+}
+fn make_copy(x: i32){
+    println!("{}", x);
+}
+
+
+fn _copy_clone_move(){
+    let s1 = String::from("well hello there!!");
+    let s2 = s1;
+
+    let z1 = String::from("well hello there!!");
+    let z2 = z1.clone();
 
     let x = 1;
-
     let y = x;
 
-    let d = "s";
+    let d = 4;
     let b = d;
 
-    println!("{}", d==b);
+    println!("  z:{}  s:{} d:{}", z1==z2, x==y, d==b);
 }
