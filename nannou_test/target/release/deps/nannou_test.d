23ff7d9d4b704ed51b2ce2a29cd799cfd94c4012
CherifiMi <cherifi154949@gmail.com>
2022-07-09 23:06:30 +0100

    nannou grid test

---
+++ nannou_test/target/release/deps/nannou_test.d
@@ -0,0 +1,5 @@
+D:\mito\Resources\rust\rust-learn\nannou_test\target\release\deps\nannou_test.exe: src\main.rs
+
+D:\mito\Resources\rust\rust-learn\nannou_test\target\release\deps\nannou_test.d: src\main.rs
+
+src\main.rs:
---
+++ nannou_test/target/release/nannou_test.d
@@ -0,0 +1,1 @@
+D:\mito\Resources\rust\rust-learn\nannou_test\target\release\nannou_test.exe: D:\mito\Resources\rust\rust-learn\nannou_test\src\main.rs
